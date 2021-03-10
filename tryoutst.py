66019cc9ec9c726fa6ed5ba6a4c04ec94d674189
Ayoush Srivastava <as1719@ic.ac.uk>
2021-03-10 14:28:19 +0000

    Update
    Update will now allow user to either access a local video to their computer or a YouTube video using a URL.

--- tryoutst.py
+++ tryoutst.py
@@ -1,24 +1,29 @@
 import streamlit as st
-import pytube
 #unsafe_allow_html = True
 #This mockup website displays the YT video they asked for
 col1, col2, col3 = st.beta_columns(3)
 
 with col2:
-    st.image("SignVid_Logo.jpg", width = 150)
+    st.image("SignVidLogo.jpg", width = 150)
 #st.title('Welcome to SignVid!')
 st.markdown("<h1 style='text-align: center; color: black;'>Welcome to SignVid!</h1>", unsafe_allow_html=True)
 #st.subheader('Enter a YouTube URL and we will translate it into British Sign Language')
 st.markdown("<p style='text-align: center; color: black;'>Enter a YouTube URL and we will translate it into British Sign Language", unsafe_allow_html=True)
 
 
-video_url = st.text_input(" ")
+# video_url = st.text_input("Enter text here")
 
-if video_url:
-    #Run the code and return the YT video
-    #Later on, this will show our video
-    st.video(video_url)
+video_file_name = st.text_input(" ")
 
+# check if file is local to directory or url link to YouTube
+if video_file_name:
+    if "youtube" not in video_file_name:
+        video_file = open(video_file_name, 'rb')
+        video_bytes = video_file.read()
+
+        st.video(video_bytes)
+    if "youtube" in video_file_name:
+        st.video(video_file_name)
 
 about = st.sidebar.beta_expander("About us")
 about.write("We are a team of students from Imperial College London who made this WebApp for our DAPP2 project")
