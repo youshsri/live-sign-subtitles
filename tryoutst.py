0b83897294cce23aca0392d3211ae44557069704
Ayoush Srivastava <as1719@ic.ac.uk>
2021-03-10 14:49:51 +0000

    Update
    Added a section that allows the user to upload a mp4 video of their choice and play it. Also added comments to the code.

--- tryoutst.py
+++ tryoutst.py
@@ -10,20 +10,34 @@ st.markdown("<h1 style='text-align: center; color: black;'>Welcome to SignVid!</
 #st.subheader('Enter a YouTube URL and we will translate it into British Sign Language')
 st.markdown("<p style='text-align: center; color: black;'>Enter a YouTube URL and we will translate it into British Sign Language", unsafe_allow_html=True)
 
-
 # video_url = st.text_input("Enter text here")
 
 video_file_name = st.text_input(" ")
 
 # check if file is local to directory or url link to YouTube
 if video_file_name:
+
+    # check if the file is not a YouTube url
     if "youtube" not in video_file_name:
         video_file = open(video_file_name, 'rb')
         video_bytes = video_file.read()
 
         st.video(video_bytes)
+    
+    # check if the file is a YouTube url
     if "youtube" in video_file_name:
         st.video(video_file_name)
+
+# allow for the upload of video files in mp4 format
+message = "Please upload a file in .mp4 format."
+uploaded_file = st.file_uploader(message, type=['mp4'])
+
+# check if uploaded file exists
+if uploaded_file:
+    
+    # if it does, read its byte data and use streamlit to access the video
+    bytes_data = uploaded_file.read()
+    st.video(bytes_data)
 
 about = st.sidebar.beta_expander("About us")
 about.write("We are a team of students from Imperial College London who made this WebApp for our DAPP2 project")
